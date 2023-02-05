d84eff172d13c4883a4cbbd9c26122e21aea94f5
mrx7355608 <mrx7355608@gmail.com>
2023-02-04 20:04:34 -0800

    modified ``insert`` method's arg type

--- src/core/interfaces/data-source-generic.interface.ts
+++ src/core/interfaces/data-source-generic.interface.ts
@@ -9,7 +9,7 @@ export interface IDataSource<T> {
     findAll(paginationData: IPaginationData): Promise<T[]>;
     findById(id: string): Promise<T | null>;
     findOne(filter: any): Promise<T | null>;
-    insert(data: T): Promise<T>;
+    insert<Y>(data: Y): Promise<T>;
     update(id: string, data: T): Promise<T>;
     deleteData(id: string): Promise<T>;
 }
