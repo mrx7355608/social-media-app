57be81df35b027c6b57ae812c9f2fd1704f0bb6b
mrx7355608 <mrx7355608@gmail.com>
2023-02-04 20:07:14 -0800

    refactored user data source

--- tests/mocks/userDataSource.ts
+++ tests/mocks/userDataSource.ts
@@ -1,45 +1,40 @@
-import { IUser } from "@/core/interfaces/user.interfaces";
+import { IUser, IUserDBModel } from "@/core/interfaces/user.interfaces";
 import { IDataSource } from "@/core/interfaces/data-source-generic.interface";
 
-let users: IUser[] = [];
+let users: IUserDBModel[] = [];
 
-export const userDB: IDataSource<IUser> = {
-    async findAll(): Promise<IUser[]> {
+export const userDB: IDataSource<IUserDBModel> = {
+    async findAll(): Promise<IUserDBModel[]> {
         return users;
     },
 
-    async findById(id: string): Promise<IUser> {
-        const user = users.filter((u) => (u as any).id === id)[0];
+    async findById(id: string): Promise<IUserDBModel> {
+        const user = users.filter((user) => user._id === id)[0];
         return user;
     },
 
-    async findOne(filter: { email: string }): Promise<IUser> {
+    async findOne(filter: { email: string }): Promise<IUserDBModel> {
         const user = users.filter((user) => user.email === filter.email)[0];
         return user;
     },
 
-    async insert(userData: IUser): Promise<IUser> {
-        // TODO: in actual implementation of user data source
-        // make sure to remove the confirmPassword field from userData
-        const newUser = {
-            _id: Date.now(),
-            createdAt: new Date(Date.now()),
-            ...userData,
-        };
-
+    async insert<IUser>(userData: IUser): Promise<IUserDBModel> {
+        const newUser = Object.assign(userData as any, {
+            createdAt: Date.now(),
+        });
         users.push(newUser);
         return newUser;
     },
 
-    async update(id, data): Promise<IUser> {
-        const user = users.filter((user) => (user as any)._id === id)[0];
+    async update(id, data): Promise<IUserDBModel> {
+        const user = users.filter((user) => user._id === id)[0];
         const updatedUser = Object.assign(user, data);
         return updatedUser;
     },
 
-    async deleteData(id): Promise<IUser> {
-        const user = users.filter((user) => (user as any)._id === id)[0];
-        users = users.filter((user) => (user as any)._id === id);
+    async deleteData(id): Promise<IUserDBModel> {
+        const user = users.filter((user) => user._id === id)[0];
+        users = users.filter((user) => user._id === id);
         return user;
     },
 };
