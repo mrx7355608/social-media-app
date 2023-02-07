53c942ff3edfc19b853a3fab73153c1f6a0318a1
mrx7355608 <mrx7355608@gmail.com>
2023-02-07 02:55:30 -0800

    updated config file

--- config/index.ts
+++ config/index.ts
@@ -1,4 +1,11 @@
 // TODO: use .env file
 export default {
     apiUrl: "http://localhost:8000/api/v1",
+    email: {
+        user: "myuser",
+        pass: "myuserpass",
+        host: "smtp.gmail.com",
+        port: 123,
+        sender: "bob@example.com",
+    },
 };
