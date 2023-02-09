dfe0022ec84ad29d021a7cd1029be8cdb6ade4b5
mrx7355608 <mrx7355608@gmail.com>
2023-02-09 15:22:36 -0800

    added controller to re-send acc verif email

---
+++ src/controllers/auth/post-verification-email.ts
@@ -0,0 +1,25 @@
+import { IUserDBModel } from "@/core/interfaces/user.interfaces";
+import { IHttpRequest } from "../interfaces/httpRequest.interface";
+
+export function postVerificationEmail({
+    sendVerificationEmail,
+}: {
+    sendVerificationEmail: (email: string) => Promise<IUserDBModel>;
+}) {
+    return async function (httpRequest: IHttpRequest) {
+        try {
+            const email = httpRequest.body.email;
+            const user = await sendVerificationEmail(email);
+            const message = `A verification email has been sent to ${user.email}`;
+            return {
+                statusCode: 200,
+                body: { message },
+            };
+        } catch (err: any) {
+            return {
+                statusCode: err.statusCode || 400,
+                body: { error: err.message },
+            };
+        }
+    };
+}
