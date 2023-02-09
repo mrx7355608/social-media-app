45b68e33b1517ffdd8f36841937179248f1a66e1
mrx7355608 <mrx7355608@gmail.com>
2023-02-09 13:25:11 -0800

    created user model

--- src/frameworks/models/user.model.ts
+++ src/frameworks/models/user.model.ts
@@ -1,6 +1,39 @@
 import { IUser } from "@/core/interfaces/user.interfaces";
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema<IUser>();
+const userSchema = new mongoose.Schema<IUser>({
+    firstname: {
+        type: String,
+        required: true,
+    },
+    lastname: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    friends: {
+        type: [String],
+        required: true,
+    },
+    pendingRequests: {
+        type: [String],
+        required: true,
+    },
+    isEmailVerified: {
+        type: Boolean,
+        required: true,
+    },
+    profilePicture: {
+        type: String,
+        required: true,
+    },
+});
 
 export const UserModel = mongoose.model<IUser>("User", userSchema);
