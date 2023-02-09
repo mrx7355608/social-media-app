8ec3ba5d6abaeaa815e9d5c412c269c0167017db
mrx7355608 <mrx7355608@gmail.com>
2023-02-09 15:39:08 -0800

    created forgot password controller

---
+++ src/controllers/auth/post-forgot-password.ts
@@ -0,0 +1,25 @@
+import { IUserDBModel } from "@/core/interfaces/user.interfaces";
+import { IHttpRequest } from "../interfaces/httpRequest.interface";
+
+export function forgotPasswordController({
+    forgotPassword,
+}: {
+    forgotPassword: (email: string) => Promise<IUserDBModel>;
+}) {
+    return async function (httpRequest: IHttpRequest) {
+        try {
+            const email = httpRequest.body.email;
+            const user = await forgotPassword(email);
+            const message = `A password reset email has been sent to ${user.email}`;
+            return {
+                statusCode: 200,
+                body: { message },
+            };
+        } catch (err: any) {
+            return {
+                statusCode: err.statusCode || 400,
+                body: { error: err.message },
+            };
+        }
+    };
+}
