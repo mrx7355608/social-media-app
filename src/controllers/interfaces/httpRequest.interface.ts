d4fc7b75c8bc1b70c8ce84c83c984ebbf85bc564
mrx7355608 <mrx7355608@gmail.com>
2023-02-09 19:12:18 -0800

    created a func that returns a req handler

---
+++ src/controllers/interfaces/httpRequest.interface.ts
@@ -0,0 +1,8 @@
+import { IUserDBModel } from "@/core/interfaces/user.interfaces";
+
+export interface IHttpRequest {
+    params: any;
+    body: any;
+    query: any;
+    user: IUserDBModel;
+}
---
+++ src/frameworks/makeRequestHandler.ts
@@ -0,0 +1,22 @@
+import { IHttpRequest } from "src/controllers/interfaces/httpRequest.interface";
+import { Request, Response, NextFunction, RequestHandler } from "express";
+
+export function makeRequest(
+    controller: (httpRequest: IHttpRequest) => Promise<any>
+): RequestHandler {
+    return function (req: Request, res: Response, next: NextFunction) {
+        const httpRequest: IHttpRequest = {
+            body: req.body,
+            params: req.params,
+            query: req.query,
+            user: (req as any).user,
+        };
+        controller(httpRequest)
+            .then((response) => {
+                return res.status(response.statusCode).json(response.body);
+            })
+            .catch((err: any) => {
+                return res.status(500).json({ error: "Something went wrong" });
+            });
+    };
+}
