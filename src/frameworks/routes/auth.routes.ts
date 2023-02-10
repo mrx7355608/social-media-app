601eb1b9e647bb73bf68c9a9ad0fc4d66b7d131a
mrx7355608 <mrx7355608@gmail.com>
2023-02-09 20:40:45 -0800

    refactored auth routes

--- src/frameworks/routes/auth.routes.ts
+++ src/frameworks/routes/auth.routes.ts
@@ -1,24 +1,16 @@
-import {
-    NextFunction,
-    RequestHandler,
-    Router,
-    Request,
-    Response,
-} from "express";
+import { Router } from "express";
 import { makeRequest } from "../makeRequestHandler";
 import { authController } from "@/controllers/auth";
-import passport from "passport";
 import isAuth from "../middlewares/isAuth";
 
 export const authRouter = Router();
 
 authRouter.post("/login", authController.login); // passportjs login
 authRouter.post("/signup", makeRequest(authController.registerUser));
-authRouter.use(isAuth);
-authRouter.get("/verify-account", makeRequest(authController.verifyAccount));
 authRouter.post("/forgot-password", makeRequest(authController.forgotPassword));
-authRouter.post("/logout", authController.logout);
+authRouter.get("/verify-account", makeRequest(authController.verifyAccount));
 authRouter.post(
     "/resend-verification-email",
     makeRequest(authController.resendAccountVerificationEmail)
 );
+authRouter.post("/logout", isAuth, authController.logout);
