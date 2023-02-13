f502feba2af3a814b1b166148afbd1edc5513e07
mrx7355608 <mrx7355608@gmail.com>
2023-02-12 22:11:02 -0800

    created reset password controller

---
+++ src/controllers/auth/post-reset-password.ts
@@ -0,0 +1,38 @@
+import { IUserDBModel } from "@/core/interfaces/user.interfaces";
+import { IHttpRequest } from "../interfaces/httpRequest.interface";
+
+export function resetPasswordController({
+    resetPassword,
+}: {
+    resetPassword: ({
+        token,
+        password,
+        confirmPassword,
+    }: {
+        token: string;
+        password: string;
+        confirmPassword: string;
+    }) => Promise<IUserDBModel>;
+}) {
+    return async function (httpRequest: IHttpRequest) {
+        try {
+            const token: string = httpRequest.query.token;
+            const password: string = httpRequest.body.password;
+            const confirmPassword: string = httpRequest.body.confirmPassword;
+            await resetPassword({
+                token,
+                password,
+                confirmPassword,
+            });
+            return {
+                statusCode: 200,
+                body: { message: "Password has been reset successfully!" },
+            };
+        } catch (err: any) {
+            return {
+                statusCode: err.statusCode || 400,
+                body: { error: err.message },
+            };
+        }
+    };
+}
