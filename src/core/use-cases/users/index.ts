124761f6fb6192331c1b72b05a81017e1ef89eca
mrx7355608 <mrx7355608@gmail.com>
2023-02-12 21:15:30 -0800

    re-arranged use cases

--- src/core/use-cases/users/index.ts
+++ src/core/use-cases/users/index.ts
@@ -1,3 +1,105 @@
-import { addUserFactory } from "./addUser";
 import { listAllUsersFactory } from "./listAllUsers";
 import { listOneUserFactory } from "./listOneUser";
+import { addUserFactory } from "./addUser";
+import { removeUserFactory } from "./removeUser";
+import { verifyAccountFactory } from "./verifyAccount";
+import { forgotPasswordFactory } from "./forgotPassword";
+import { sendFriendRequestFactory } from "./sendFriendReq";
+import { acceptRequestFactory } from "./acceptRequest";
+import { rejectRequestFactory } from "./rejectRequest";
+import { updateProfilePictureFactory } from "./updateProfilePicture";
+import { sendVerificationEmailFactory } from "./sendVerificationEmail";
+import { resetPasswordFactory } from "./resetPassword";
+
+// Services and other helper functions
+import { ErrorServices } from "@/services/error.services";
+import { HashServices } from "@/services/hash.services";
+import { JwtServices } from "@/services/jwt.services";
+import { EmailServices } from "@/services/email.services";
+import { UserDataSource } from "@/data/user.data";
+import validator from "validator";
+
+const errorServices = new ErrorServices();
+const hashServices = new HashServices();
+const jwtServices = new JwtServices();
+const emailServices = new EmailServices();
+const userDataSource = new UserDataSource();
+const isMongoId = validator.isMongoId;
+
+const listAllUsers = listAllUsersFactory({
+    userDataSource,
+});
+const listOneUser = listOneUserFactory({
+    userDataSource,
+    errorServices,
+    isMongoId,
+});
+const addUser = addUserFactory({
+    userDataSource,
+    errorServices,
+    hashServices,
+    emailServices,
+});
+const removeUser = removeUserFactory({
+    userDataSource,
+    errorServices,
+    isMongoId,
+});
+const verifyAccount = verifyAccountFactory({
+    userDataSource,
+    errorServices,
+    jwtServices,
+});
+const forgotPassword = forgotPasswordFactory({
+    userDataSource,
+    errorServices,
+    emailServices,
+    emailValidator: validator.isEmail,
+});
+const acceptRequest = acceptRequestFactory({
+    userDataSource,
+    errorServices,
+    isMongoId,
+});
+const sendFriendReq = sendFriendRequestFactory({
+    userDataSource,
+    errorServices,
+    isMongoId,
+});
+const rejectRequest = rejectRequestFactory({
+    userDataSource,
+    errorServices,
+    isMongoId,
+});
+const updateProfilePicture = updateProfilePictureFactory({
+    userDataSource,
+    errorServices,
+    isValidUrl: validator.isURL,
+});
+const sendVerificationEmail = sendVerificationEmailFactory({
+    userDataSource,
+    errorServices,
+    emailServices,
+    emailValidator: validator.isEmail,
+});
+const resetPassword = resetPasswordFactory({
+    userDataSource,
+    errorServices,
+    hashServices,
+    jwtServices,
+});
+
+export const userServices = {
+    listAllUsers,
+    listOneUser,
+    addUser,
+    removeUser,
+    verifyAccount,
+    forgotPassword,
+    acceptRequest,
+    rejectRequest,
+    sendFriendReq,
+    updateProfilePicture,
+    sendVerificationEmail,
+    resetPassword,
+};
