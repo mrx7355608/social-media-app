3be17d733a9e832dbbc4b0e2a733e62f47fde9c0
mrx7355608 <mrx7355608@gmail.com>
2023-02-14 19:57:50 -0800

    updated routes

--- src/frameworks/routes/user.routes.ts
+++ src/frameworks/routes/user.routes.ts
@@ -7,25 +7,23 @@ export const userRouter = Router();
 
 userRouter.use(isAuth);
 userRouter.get("/", makeRequest(userControllers.getUsers));
+userRouter.get("/me", makeRequest(userControllers.getCurrentUser));
 userRouter.get("/:id", makeRequest(userControllers.getOneUser));
 userRouter.post(
     "/send-friend-request/:id",
     makeRequest(userControllers.sendReq)
 );
 userRouter.patch(
-    "/pending-requests/accept/:id",
+    "/me/pending-requests/accept/:id",
     makeRequest(userControllers.acceptRequest)
 );
 userRouter.patch(
-    "/pending-requests/reject/:id",
+    "/me/pending-requests/reject/:id",
     makeRequest(userControllers.rejectRequest)
 );
 // userRouter.get("/:id/photos");
 // userRouter.get("/:id/friends");
 // userRouter.post("/send-friend-request/:id");
-// userRouter.get("/me");
 // userRouter.get("/me/photos");
 // userRouter.get("/me/friends");
 // userRouter.get("/me/pending-requests");
-// userRouter.patch("/me/pending-requests/:id/reject");
-// userRouter.patch("/me/pending-requests/:id/accept");
