6f8d3e9cdcbe7080d80de25cc76a315cc89ccb1a
mrx7355608 <mrx7355608@gmail.com>
2023-02-17 21:50:09 -0800

    created an interface for mongoose models

--- src/core/entities/comment.interfaces.ts
+++ src/core/entities/comment.interfaces.ts
@@ -1,6 +1,12 @@
 import { IAuthor } from "./post.interfaces";
+import { Document } from "mongoose";
 
 export interface IComment {
     author: IAuthor;
     text: string;
+}
+
+export interface ICommentDBModel extends IComment, Document {
+    createdAt: Date;
+    updatedAt: Date;
 }
