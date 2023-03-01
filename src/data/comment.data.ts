f728250464e171c4f2bd1f2e05e6416c46c3fdb9
mrx7355608 <mrx7355608@gmail.com>
2023-02-28 19:34:58 -0800

    refactored comment data source

--- src/data/comment.data.ts
+++ src/data/comment.data.ts
@@ -1,36 +1,21 @@
-import { IComment, ICommentDBModel } from "@/core/entities/comment.interfaces";
-import {
-    IDataSource,
-    IPaginationData,
-} from "@/core/interfaces/data-source-generic.interface";
+import { IComment } from "@/core/entities/comment.interfaces";
 import { CommentModel } from "@/frameworks/models/comment.model";
 
-export class CommentDataSource implements IDataSource<ICommentDBModel> {
-    async findAll(paginationData: IPaginationData): Promise<ICommentDBModel[]> {
-        return await CommentModel.find();
+export class CommentDataSource {
+    async insert(data: IComment): Promise<IComment> {
+        const newComment = await CommentModel.create(data);
+        return newComment;
     }
 
-    async findOne(filter: any): Promise<ICommentDBModel | null> {
-        const post = await CommentModel.findOne(filter);
-        return post as ICommentDBModel;
+    async update(commentId: string, changes: IComment): Promise<IComment> {
+        const updatedComment = await CommentModel.findByIdAndUpdate(
+            commentId,
+            changes
+        );
+        return updatedComment as IComment;
     }
-
-    async findById(id: string): Promise<ICommentDBModel | null> {
-        const post = await CommentModel.findById(id);
-        return post as ICommentDBModel;
-    }
-
-    async insert<Y>(data: Y): Promise<ICommentDBModel> {
-        return (await CommentModel.create(data)) as ICommentDBModel;
-    }
-
-    async update<Y>(id: string, data: Y): Promise<ICommentDBModel> {
-        return (await CommentModel.findByIdAndUpdate(id, data as IComment, {
-            new: true,
-        })) as ICommentDBModel;
-    }
-
-    async deleteData(id: string): Promise<ICommentDBModel> {
-        return (await CommentModel.findByIdAndDelete(id)) as ICommentDBModel;
+    async deleteComment(commentId: string): Promise<IComment> {
+        const deletedComment = await CommentModel.findByIdAndDelete(commentId);
+        return deletedComment as IComment;
     }
 }
